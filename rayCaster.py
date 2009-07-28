feea15f923c25e33bb185a21b4a6603eb261a419
Scott Graham Mansell <sgm53@cosc757.cosc.canterbury.ac.nz>
2009-07-28 17:47:00 +1200

    Made more oo

--- rayCaster.py
+++ rayCaster.py
@@ -1,3 +1,4 @@
+import cProfile
 from geom3 import Point3, Vector3, Ray3, cross, dot, unit, length
 from math import sqrt, tan
 from colour import Colour
@@ -21,7 +22,7 @@ BACKGROUND = Colour(0.6,0.6,0.6)            # Colour of the background
 
 SHINY_RED = Material(Colour(0.7, 0.3, 0.2), Colour(0.4,0.4,0.4), 100)
 SHINY_BLUE = Material(Colour(0.2, 0.3, 0.7), Colour(0.8,0.8,0.8), 200)
-MATT_GREEN = Material(Colour(0.1, 0.7, 0.1), None, None)
+MATT_GREEN = Material(Colour(0.1, 0.7, 0.1), Colour(0.0,0.2,0.0), None)
 
 EYEPOINT = Point3(0.5, 0.5, 2)
 
@@ -29,21 +30,11 @@ SCENE = Scene([Sphere(Point3(0.35,0.6,0.5), 0.25, SHINY_BLUE),
                Sphere(Point3(0.75,0.2,0.6), 0.15, SHINY_RED),
                Plane(Point3(0,0,0), Vector3(0,1,0), MATT_GREEN)])
 
-# Main body. Set up an image then compute colour at each pixel
+class rayCaster(object):
+    def __init__(self):
+	return
 
-img = Image.new("RGB", (WIN_SIZE, WIN_SIZE))
-
-for row in range(WIN_SIZE):
-    for col in range(WIN_SIZE):
-		
-        pixelCentre = Point3((col + 0.5) * SPACING, ((WIN_SIZE -row) + 0.5) * SPACING, 1)
-        rayDir = pixelCentre - EYEPOINT
-        ray = Ray3(EYEPOINT, rayDir)
-
-        # Compute the ray from the eye through the centre
-        # of pixel (col, row)
-        # ***** DO IT!!! ******
-
+    def getColour(self, ray):
         hitPoint = SCENE.intersect(ray)
 
         if hitPoint is None:
@@ -51,10 +42,10 @@ for row in range(WIN_SIZE):
         else:
             (obj, t) = hitPoint
             surface = obj.material
-            
+        
             normal = obj.normal(ray.pos(t))
-            view = -rayDir
-            		
+            view = -ray.dir
+                    
             # Shadow Test
             sray = Ray3(ray.pos(t), LIGHT_DIR)
             shadowTest = SCENE.intersect(sray)
@@ -66,7 +57,26 @@ for row in range(WIN_SIZE):
                 colour = surface.litColour(normal, AMBIENT, LIGHT_DIR, LIGHT_COLOUR, view)
             else:
                 colour = surface.litColour(normal, AMBIENT, LIGHT_DIR, None, view)
+	return colour
+        
+    # Main body. Set up an image then compute colour at each pixel
+    def trace(self):
+        img = Image.new("RGB", (WIN_SIZE, WIN_SIZE))
 
-        img.putpixel((col, row), colour.intColour())
+        for row in range(WIN_SIZE):
+            for col in range(WIN_SIZE):
+                
+                pixelCentre = Point3((col + 0.5) * SPACING, ((WIN_SIZE -row) + 0.5) * SPACING, 1)
+                rayDir = pixelCentre - EYEPOINT
+                ray = Ray3(EYEPOINT, rayDir)
 
-img.save("out.png")  # Display image in default image-viewer application
+                # Compute the ray from the eye through the centre
+                # of pixel (col, row)
+                
+
+                img.putpixel((col, row), self.getColour(ray).intColour())
+
+        img.save("out.png")  # Display image in default image-viewer application
+
+caster = rayCaster()
+caster.trace()

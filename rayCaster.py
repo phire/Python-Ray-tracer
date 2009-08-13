bf4bfafb6c094360b3043b6c9770eba37bd6e876
phire <phire@Nirrit.(none)>
2009-08-14 11:58:07 +1200

    Split out stages of rays

--- rayCaster.py
+++ rayCaster.py
@@ -45,9 +45,9 @@ SCENE = Scene([
 	       ])
 
 SCENE.lights = [
-	  #Light(SCENE, unit(Vector3(2,5,3)), Colour(0.8, 0.8, 0.8)),
-	  Light(SCENE, unit(Vector3(-4,5,0)), Colour(0.3, 0.3, 0.3)),
-	  #PointLight(SCENE, Point3(.7, .5, .8), Colour(0.8, 0.8, 0.8)),
+	  Light(SCENE, unit(Vector3(2,5,3)), Colour(0.4, 0.4, 0.0)),
+	  Light(SCENE, unit(Vector3(-4,3,0)), Colour(0.3, 0.0, 0.0)),
+	  PointLight(SCENE, Point3(.7, .5, .8), Colour(0.8, 0.8, 0.8)),
 	  ]
 SCENE.background = Colour(0, 0, 0)
 SCENE.ambient = Colour(0.1, 0.1, 0.1) 
@@ -57,42 +57,83 @@ class rayCaster(object):
 	return
 
     def rayColour(self, ray, depth=0):
-	if depth > 100:
-	    print "Max Depth reached!"
-	    return Colour(0,0,0)
 	
         hit = SCENE.intersect(ray)
-        if hit is None:
-            return SCENE.background
-        else:
-	    hit.calcReflections(SCENE)
-	    hit.calcLights(SCENE)
-	    return hit.colour()
+	return hit
 	    
     # Main body. Set up an image then compute colour at each pixel
     def trace(self):
         img = Image.new("RGB", (WIN_SIZE, WIN_SIZE))
 	aa = NoAA(EYEPOINT, self.rayColour)
-	print "\tTracing Rays...   0%",
+	print "\tTracing Primary Rays...   0%",
 	sys.stdout.flush()
 	count = 0
 	max = float(WIN_SIZE**2)
 	lastPercentage = 0
+	hits = []
         for row in range(WIN_SIZE):
+	    ROW = []
             for col in range(WIN_SIZE):
                 count += 1
 
 		pixelBox = (col * SPACING, (WIN_SIZE - row) * SPACING, (col+1) * SPACING, (WIN_SIZE - row+1) * SPACING)
 
-                img.putpixel((col, row), aa.getPixel(pixelBox).intColour())
+                ROW.append(aa.getPixel(pixelBox))
 	    percentage = (count / max * 100)
+	    hits.append(ROW)
 	    if percentage - lastPercentage > .9:
 	        print "\b\b\b\b\b\b%4.0f%%" % percentage,
 		sys.stdout.flush()
 		lastPercentage = percentage
 	print "\b\b\b\b\b\b Done"
+
+	print "\tTracing Reflection Rays...   0%",
+	count = 0
+	lastPercentage = 0
+	for cols in hits:
+	    for pixel in cols:
+	        count += 1
+		if pixel: pixel.calcReflections(SCENE)
+	    percentage = (count / max * 100)
+	    if percentage - lastPercentage > 3.3:
+	        print "\b\b\b\b\b\b%4.0f%%" % percentage,
+		sys.stdout.flush()
+		lastPercentage = percentage
+	print "\b\b\b\b\b\b Done"
+
+	print "\tTracing Shadow Rays...   0%",
+	count = 0
+	lastPercentage = 0
+	for cols in hits:
+	    for pixel in cols:
+	        count += 1
+	        if pixel: pixel.calcLights(SCENE)
+	    percentage = (count / max * 100)
+	    if percentage - lastPercentage > 3.3:
+	        print "\b\b\b\b\b\b%4.0f%%" % percentage,
+		sys.stdout.flush()
+		lastPercentage = percentage
+	print "\b\b\b\b\b\b Done"
+
+	print "\tRendering Image...   0%",
+	count = 0
+	lastPercentage = 0
+	for row in range(WIN_SIZE):
+	    for col in range(WIN_SIZE):
+	        count += 1
+		pixel = hits[row][col]
+		if pixel:
+		    img.putpixel((col,row), hits[row][col].colour().intColour())
+		else: img.putpixel((col,row), SCENE.background.intColour())
+	    percentage = (count / max * 100)
+	    if percentage - lastPercentage > 9.9:
+	        print "\b\b\b\b\b\b%4.0f%%" % percentage,
+		sys.stdout.flush()
+		lastPercentage = percentage
+	print "\b\b\b\b\b\b Done"
+
         img.save("out.png")  # Display image in default image-viewer application
 
 caster = rayCaster()
-#cProfile.run("caster.trace()")
-caster.trace()
+cProfile.run("caster.trace()")
+#caster.trace()

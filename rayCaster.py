2e9f532c71ee054dd1f15b919612c62ae11c15d8
Scott Mansell <phiren@gmail.com>
2009-08-19 15:39:19 +1200

    Moved scene definition stuff to an external python file

--- rayCaster.py
+++ rayCaster.py
@@ -1,58 +1,17 @@
 #!/usr/bin/python
 import cProfile
 from antiAlaising import *
-from geom3 import Point3, Vector3, Ray3, cross, dot, unit, length
-from math import sqrt, tan
-from colour import Colour
-from sphere import Sphere
-from plane import Plane
 import Image
-from material import Material
-from scene import Scene
-from light import *
-from texture import *
-from CSG import *
 import sys
 from Tkinter import Tk, Canvas, PhotoImage
 from camera import Camera
 
+sys.path.insert(0,"scenes/")
 
-# Define various scene constants
+#import balls as definition
+definition = __import__("balls")
 
-WIN_SIZE = 400                              # Screen window size (square)
-SPACING = 1.0 / WIN_SIZE                    # Pixel spacing on viewplane
-
-SHINY_RED = Material(Colour(0.7, 0.1, 0.2), Colour(0.4,0.4,0.4), 100, .2)
-SHINY_BLUE = Material(Colour(0.2, 0.3, 0.7), Colour(0.8,0.8,0.8), 200, .3)
-MATT_GREEN = Material(Colour(0.1,0.85, 0.1))
-CHECK_FLOOR = Material(None, None, None, None, Texture_Check(6, Colour(0,0,0), Colour(0.5,0.5,0.5)))
-
-EYEPOINT = Point3(0.0, 0.1, 0.9)
-
-SCENE = Scene([
-	       Sphere(Point3(0.35,0.6,0.5), 0.25, SHINY_BLUE),
-	       Difference([
-	       Intersection([ # Cube
-		  #Plane(Point3(0.2,0.0,0.5), Vector3(0,-1,0), CHECK_FLOOR),
-		  Plane(Point3(0.1,0.175,0.8), Vector3(0, 1,.25), SHINY_BLUE),
-		  #Plane(Point3(0.1,0.1,0.5), Vector3(-1,0,0), CHECK_FLOOR),
-		  #Plane(Point3(0.4,0.1,0.5), Vector3( 1,0,0), CHECK_FLOOR),
-		  #Plane(Point3(0.5,0.1,0.8), Vector3(0,0, 1), CHECK_FLOOR),
-		  #Plane(Point3(0.5,0.1,0.5), Vector3(0,0,-1), CHECK_FLOOR),
-		  Sphere(Point3(0.1,0.175,0.8), 0.175, SHINY_BLUE),
-			  ]),
-		 Sphere(Point3(0.1,0.175,0.8), 0.125, SHINY_BLUE)]),
-	       Sphere(Point3(0.75,0.15,.2), 0.15, SHINY_RED),
-               Plane(Point3(0,0,0), Vector3(0,1,0), CHECK_FLOOR)
-	       ])
-
-SCENE.lights = [
-	  #Light(SCENE, unit(Vector3(2,5,3)), Colour(0.6, 0.6, 0.6)),
-	  #Light(SCENE, unit(Vector3(-4,3,0)), Colour(0.7, 0.7, 0.7)),
-	  PointLight(SCENE, Point3(.5, 1.1, 1.2), Colour(0.9, 0.9, 0.9)),
-	  ]
-SCENE.background = Colour(0, 0, 0)
-SCENE.ambient = Colour(0.1, 0.1, 0.1) 
+WIN_SIZE = definition.camera.size
 
 class rayCaster(object):
     def __init__(self):
@@ -80,22 +39,20 @@ class rayCaster(object):
         self.image.put(rowColourString, to=(0, row))
         self.root.update()
 
-    def rayColour(self, ray, depth=0):
+    def rayColour(self, ray, scene):
 	
-        hit = SCENE.intersect(ray)
-	hit.calcReflections(SCENE)
-	hit.calcLights(SCENE)
+        hit = scene.intersect(ray)
+	hit.calcReflections(scene)
+	hit.calcLights(scene)
 	return hit.colour()
 	    
     # Main body. Set up an image then compute colour at each pixel
     def trace(self):
-        img = Image.new("RGB", (WIN_SIZE, WIN_SIZE))
-	aa = NoAA(EYEPOINT, self.rayColour)
+    	camera = definition.camera
+
+        img = Image.new("RGB", (camera.size, camera.size))
 	print "\tTracing Rays...   0%",
 	sys.stdout.flush()
-
-	camera = Camera(EYEPOINT,WIN_SIZE)
-	camera.lookAt(Point3(0.5,0.0,0.5))
 
 	count = 0
 	max = float(WIN_SIZE**2)
@@ -105,9 +62,7 @@ class rayCaster(object):
             for col in range(WIN_SIZE):
                 count += 1
 
-		#pixelBox = (col * SPACING, (WIN_SIZE - row) * SPACING, (col+1) * SPACING, (WIN_SIZE - row+1) * SPACING)
-		#pixel = aa.getPixel(pixelBox)
-		pixel = self.rayColour(camera.getRay(col, row))
+		pixel = self.rayColour(camera.getRay(col, row), definition.scene)
                 img.putpixel((col, row), pixel.intColour())
 		ROW.append(pixel)
 	    percentage = (count / max * 100)
@@ -121,5 +76,5 @@ class rayCaster(object):
         img.save("out.png")  # Display image in default image-viewer application
 
 caster = rayCaster()
-#cProfile.run("caster.trace()")
-caster.root.mainloop()
+cProfile.run("caster.root.mainloop()")
+#caster.root.mainloop()
